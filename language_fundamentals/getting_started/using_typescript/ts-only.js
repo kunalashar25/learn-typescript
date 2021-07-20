aac105039d9396c4413b2c4513cfaab52e3722c9
Kunal Ashar <kunalashar@Kunals-MacBook-Pro.local>
2021-07-20 15:07:08 +0530

    folder structure updated

---
+++ language_fundamentals/getting_started/using_typescript/ts-only.js
@@ -0,0 +1,15 @@
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1");
+var input2 = document.getElementById("num2");
+// Exclamation (!) tells TS that we will always find an element and it'll never be null
+// as HTMLInputELement is written for Type casting as all html element will not have value attribute
+function add(num1, num2) {
+    return num1 + num2;
+}
+button.addEventListener("click", function () {
+    //console.log(add(input1.value, input2.value));
+    // Argument of type 'string' is not assignable to parameter of type 'number'.
+    console.log(add(+input1.value, +input2.value));
+});
+// Compile code using command
+// tsc $fileName.ts
