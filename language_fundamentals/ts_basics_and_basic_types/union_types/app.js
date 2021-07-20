3d8e6b9236394d499c08399708aa7525ec301745
Kunal Ashar <kunalashar@Kunals-MacBook-Pro.local>
2021-07-20 23:03:46 +0530

    union types

---
+++ language_fundamentals/ts_basics_and_basic_types/union_types/app.js
@@ -0,0 +1,16 @@
+// union type is used to be more flexible w.r.t types rather than being rigid
+// can take input of multiple types
+function combine(n1, n2) {
+    // return n1 + n2; Operator '+' cannot be applied to types 'string | number' and 'string | number'.ts(2365)
+    var result;
+    // if we are using multiple types then we also need to do runtime checking
+    if (typeof n1 === 'number' && typeof n2 === 'number')
+        result = n1 + n2;
+    else
+        result = "" + n1 + n2;
+    return result;
+}
+console.log(combine(5, 1.5));
+console.log(combine('Hello', 'TS'));
+console.log(combine('Hello', 15));
+console.log(combine(123, 'TS'));
---
+++ language_fundamentals/ts_basics_and_basic_types/union_types/app.ts
@@ -0,0 +1,19 @@
+// union type is used to be more flexible w.r.t types rather than being rigid
+// can take input of multiple types
+
+function combine(n1: number | string, n2: number | string) {
+	// return n1 + n2; Operator '+' cannot be applied to types 'string | number' and 'string | number'.ts(2365)
+
+	let result;
+
+	// if we are using multiple types then we also need to do runtime checking
+	if (typeof n1 === 'number' && typeof n2 === 'number') result = n1 + n2;
+	else result = `${n1}${n2}`;
+
+	return result;
+}
+
+console.log(combine(5, 1.5)); // 6.5
+console.log(combine('Hello', 'TS')); // HelloTS
+console.log(combine('Hello', 15)); // Hello15
+console.log(combine(123, 'TS')); // 123TS
